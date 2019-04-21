561a91063370b946ef41dcfa3ffe5d3cce81be4b
Alex <dragnir25@mail.ru>
2019-04-21 23:03:54 +0200

    refactor to JPA only test

--- backend/src/test/java/ru/buyanov/stylist/SlotDefinitionDatabaseTest.java
+++ backend/src/test/java/ru/buyanov/stylist/SlotDefinitionDatabaseTest.java
@@ -4,23 +4,23 @@ import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.buyanov.stylist.repository.SlotDefinitionRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 @AutoConfigureEmbeddedDatabase
 public class SlotDefinitionDatabaseTest {
     @Autowired
     private SlotDefinitionRepository repository;
 
     @Test
-    void test_allSlotDefinitionsArePresent() throws Exception {
+    void test_allSlotDefinitionsArePresent() {
         assertEquals(16, repository.findAll().size());
     }
 }
