d879610ce349504c0defb25c371f526c5a5fc61a
Alex <dragnir25@mail.ru>
2019-04-21 13:55:08 +0200

    add mapper to reduce verbosity

---
+++ backend/src/main/java/ru/buyanov/stylist/mapper/AppointmentMapper.java
@@ -0,0 +1,10 @@
+package ru.buyanov.stylist.mapper;
+
+import org.mapstruct.Mapper;
+import ru.buyanov.stylist.dto.AppointmentDto;
+import ru.buyanov.stylist.model.Appointment;
+
+@Mapper(componentModel = "spring")
+public interface AppointmentMapper {
+    Appointment create(AppointmentDto dto);
+}
--- backend/src/main/java/ru/buyanov/stylist/service/AppointmentService.java
+++ backend/src/main/java/ru/buyanov/stylist/service/AppointmentService.java
@@ -3,6 +3,7 @@ package ru.buyanov.stylist.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.buyanov.stylist.dto.AppointmentDto;
+import ru.buyanov.stylist.mapper.AppointmentMapper;
 import ru.buyanov.stylist.model.Appointment;
 import ru.buyanov.stylist.repository.AppointmentRepository;
 
@@ -10,9 +11,10 @@ import ru.buyanov.stylist.repository.AppointmentRepository;
 @AllArgsConstructor
 public class AppointmentService {
     private final AppointmentRepository repository;
+    private final AppointmentMapper mapper;
 
     public void createAppointment(AppointmentDto dto) {
-        Appointment appointment = new Appointment(dto.getSlotDefinitionId(), dto.getStylistId(), dto.getDate(), dto.getCustomerId());
+        Appointment appointment = mapper.create(dto);
         repository.save(appointment);
     }
 }
