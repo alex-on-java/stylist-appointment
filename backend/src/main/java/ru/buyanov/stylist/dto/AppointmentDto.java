a2aba141cd48f18a0c50e8eb018e9c635df1c0fd
Alex <dragnir25@mail.ru>
2019-04-21 20:34:18 +0200

    move to ISO date format

--- backend/src/main/java/ru/buyanov/stylist/dto/AppointmentDto.java
+++ backend/src/main/java/ru/buyanov/stylist/dto/AppointmentDto.java
@@ -1,6 +1,5 @@
 package ru.buyanov.stylist.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,6 @@ public class AppointmentDto {
     int id;
     int slotDefinitionId;
     int stylistId;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
     LocalDate date;
     int customerId;
 }
